a03a19fa40b15b97a8da5e3269b94ca159a12888
Paula <pbuiles1026@cue.edu.co>
2023-05-21 18:08:49 -0500

    Changes by User

---
+++ src/main/java/co/edu/cue/proyectoNuclear/mapping/dtos/AdministrativeDto.java
@@ -0,0 +1,11 @@
+package co.edu.cue.proyectoNuclear.mapping.dtos;
+
+public record AdministrativeDto(String name,
+                                String id,
+                                String email
+                                ) {
+
+
+
+
+}
