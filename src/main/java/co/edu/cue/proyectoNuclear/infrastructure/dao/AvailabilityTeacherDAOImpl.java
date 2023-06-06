39d9261ce4ec12cb1dcded9a355bb8e067799bd9
Paula <pbuiles1026@cue.edu.co>
2023-06-05 20:03:07 -0500

    Changes by Samuel v18

---
+++ src/main/java/co/edu/cue/proyectoNuclear/infrastructure/dao/AvailabilityTeacherDAOImpl.java
@@ -0,0 +1,30 @@
+package co.edu.cue.proyectoNuclear.infrastructure.dao;
+
+import java.util.List;
+
+public class AvailabilityTeacherDAOImpl implements GeneralDAO{
+    @Override
+    public List getTableList() {
+        return null;
+    }
+
+    @Override
+    public Object findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Object entity) {
+
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
